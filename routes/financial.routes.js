267bf78b94231baa62d16b092b51992fe300baa8
Yago Matias <yagomatiasdev@gmail.com>
2025-08-26 09:18:49 -0300

    correcao da rota fluxocaixa

--- routes/financial.routes.js
+++ routes/financial.routes.js
@@ -524,8 +524,7 @@ router.get('/fluxo-caixa',
         fd.vl_rateio,
         fd.in_aceite,
         fd.cd_despesaitem,
-        fd.cd_ccusto,
-        fd.nm_despesaitem
+        fd.cd_ccusto
       FROM vr_fcp_despduplicatai fd
       WHERE fd.dt_liq BETWEEN $1 AND $2
         AND fd.cd_empresa IN (${empresaPlaceholders})
@@ -552,8 +551,7 @@ router.get('/fluxo-caixa',
         fd.vl_rateio,
         fd.in_aceite,
         fd.cd_despesaitem,
-        fd.cd_ccusto,
-        fd.nm_despesaitem
+        fd.cd_ccusto
       FROM vr_fcp_despduplicatai fd
       WHERE fd.dt_liq BETWEEN $1 AND $2
         AND fd.cd_empresa IN (${empresaPlaceholders})
